2f9f6871a5e37595eb35a667f1c36ab77edbca31
Tejeswar073 <tejeswararao@pinacalabs.com>
2025-05-11 22:55:07 +0530

    css added to navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -9,23 +9,19 @@ export default function Navbar({ user }) {
 
     const handleLogout = () => {
         Cookies.remove('access_token');
-        // clear other cookies if any
-        // redirect to login
         window.location.href = '/';
     };
     return (
         <nav className="bg-gray-800 text-white p-4 flex justify-between">
-            <div className="logo font-bold">TASK MANAGER</div>
-            <div className="relative">
-                <button onClick={toggleMenu} className="focus:outline-none">
-                    <CgProfile className='bg-gray-800 rounded-full'/>
-                </button>
+            <div className="logo font-bold text-2xl flex justify-center items-center">TASK MANAGER</div>
+            <div className="relative pb-0">
+                <CgProfile onClick={toggleMenu} className='bg-gray-800 rounded-full w-8 h-8 cursor-pointer' />
                 {open && (
                     <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded shadow-lg">
-                        <div className="px-4 py-2 border-b">{user.name}</div>
+                        <div className="px-4 py-2 border-b cursor-pointer">{user.name}</div>
                         <button
                             onClick={handleLogout}
-                            className="w-full text-left px-4 py-2 hover:bg-gray-100"
+                            className="w-full text-left px-4 py-2 hover:bg-gray-800 hover:text-white cursor-pointer"
                         >
                             Sign Out
                         </button>

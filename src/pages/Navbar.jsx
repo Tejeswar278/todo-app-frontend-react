22de8dba85416f8b3de64d3af619a18a0961a3f9
Tejeswar073 <tejeswararao@pinacalabs.com>
2025-05-12 23:27:08 +0530

    added notificatin and calender icons

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Cookies from 'js-cookie';
 import { CgProfile } from "react-icons/cg";
+import serach_icon from '../../public/SearchICon.png'
+import notification_icon from '../../public/Notifications.png'
+import calender_icon from '../../public/Cal.png'
 
 export default function Navbar({ user }) {
     const [open, setOpen] = useState(false);
@@ -13,8 +16,18 @@ export default function Navbar({ user }) {
     };
     return (
         <nav className="bg-gray-800 text-white p-4 flex justify-between">
-            <div className="logo font-bold text-2xl flex justify-center items-center">TASK MANAGER</div>
-            <div className="relative pb-0">
+            <div className="logo font-bold text-2xl flex justify-center items-center">Dashboard</div>
+            <div className='flex '><input type='text' className='w-98 bg-white'/><img src={serach_icon}/></div>
+            <div className='flex gap-2'>
+                <img src={notification_icon}/>
+                <img src={calender_icon} />
+                <div>
+                    <div>Tuesday</div>
+                    <div>12/05/2025</div>
+                </div>
+
+            </div>
+            {/* <div className="relative pb-0">
                 <CgProfile onClick={toggleMenu} className='bg-gray-800 rounded-full w-8 h-8 cursor-pointer' />
                 {open && (
                     <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded shadow-lg">
@@ -27,7 +40,7 @@ export default function Navbar({ user }) {
                         </button>
                     </div>
                 )}
-            </div>
+            </div> */}
         </nav>
     );
 }

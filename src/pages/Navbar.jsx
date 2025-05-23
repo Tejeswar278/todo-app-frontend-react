6f5d634f48ba90add5c4a32e714a1beba994413c
Tejeswar073 <tejeswararao@pinacalabs.com>
2025-05-23 11:18:42 +0530

    made navbar as sticky

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,21 +1,11 @@
 import React, { useState } from 'react';
-import Cookies from 'js-cookie';
-import { CgProfile } from "react-icons/cg";
 import serach_icon from '../../public/SearchICon.png'
 import notification_icon from '../../public/Notifications.png'
 import calender_icon from '../../public/Cal.png'
 
 export default function Navbar({ user }) {
-    const [open, setOpen] = useState(false);
-
-    const toggleMenu = () => setOpen(prev => !prev);
-
-    const handleLogout = () => {
-        Cookies.remove('access_token');
-        window.location.href = '/';
-    };
     return (
-        <nav className="bg-gray-800 text-white p-4 flex justify-between">
+        <nav className="fixed top-0 left-0 w-full z-50 bg-gray-800 text-white p-4 flex justify-between">
             <div className="logo font-bold text-2xl flex justify-center items-center">Dashboard</div>
             <div className='flex '><input type='text' className='w-98 bg-white'/><img src={serach_icon}/></div>
             <div className='flex gap-2'>
@@ -27,20 +17,6 @@ export default function Navbar({ user }) {
                 </div>
 
             </div>
-            {/* <div className="relative pb-0">
-                <CgProfile onClick={toggleMenu} className='bg-gray-800 rounded-full w-8 h-8 cursor-pointer' />
-                {open && (
-                    <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded shadow-lg">
-                        <div className="px-4 py-2 border-b cursor-pointer">{user.name}</div>
-                        <button
-                            onClick={handleLogout}
-                            className="w-full text-left px-4 py-2 hover:bg-gray-800 hover:text-white cursor-pointer"
-                        >
-                            Sign Out
-                        </button>
-                    </div>
-                )}
-            </div> */}
         </nav>
     );
 }
